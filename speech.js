8c65cc41a92c7aeb1ee7a7886b1d4305f52e2058
aravindrk7 <33639370+aravindrk7@users.noreply.github.com>
2019-07-14 13:19:43 +0530

    repeat it

---
+++ speech.js
@@ -0,0 +1,10 @@
+const speak = (text) => {
+    var msg = new SpeechSynthesisUtterance(text);
+    msg.voice = window.speechSynthesis.getVoices()[3];
+    msg.voiceURI = 'native';
+    window.speechSynthesis.speak(msg);
+       console.log(msg);
+}
+
+// need to call this once to load the voices before hand
+window.speechSynthesis.getVoices();
